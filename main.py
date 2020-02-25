96111d94da92750055b23bc778443a3260c03a58
badmintonstem <jhope@badmintonschool.co.uk>
2020-02-25 09:59:04 +0000

    Score tracking

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+score = 0 # initialise the score variable, it should be 0 at the start
+
 print("What is 2 + 2?")
 answer = input ()
 answer = int(answer)
 if answer == 4:
     print("Well done")
+    score = score + 1 # Increase the score by 1
 else:
     print("Sorry the answer was 4")
 
-print("What is 4 * 6?")
+print("What is 4 x 6?")
 answer = input ()
 answer = int(answer)
 if answer == 24:
     print("Well done")
+    score = score + 1
 else:
-    print("Sorry the answer was 4")
+    print("Sorry the answer was 24")
+
+print(score)
