a7a377b81bae6f0f7e4bde10e63cc73879358d2e
badmintonstem <jhope@badmintonschool.co.uk>
2020-02-25 09:57:21 +0000

    2nd Question

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ if answer == 4:
     print("Well done")
 else:
     print("Sorry the answer was 4")
+
+print("What is 4 * 6?")
+answer = input ()
+answer = int(answer)
+if answer == 24:
+    print("Well done")
+else:
+    print("Sorry the answer was 4")
